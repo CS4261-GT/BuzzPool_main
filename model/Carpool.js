dd36318729794c5456a8fa41b60c32f1921a742a
Joe Chen <joechenzhao1@gmail.com>
2023-04-21 13:23:34 -0400

    blacklist check enabled

--- model/Carpool.js
+++ model/Carpool.js
@@ -1,10 +1,11 @@
+import { tripStatus } from "../constants/constants"
 export default class Carpool {
   constructor(
     // requried fields for creation
     title, departureTime, departureLocation, destination,
     requireDriver = true, capacity = 4, userGTIDs = [], driverGTID = "", userIDs = [],
     // auto set fields
-    tripStatus = "Not started", isTransactionFinished = false) {
+    myTripStatus = "Not started", isTransactionFinished = false) {
     this.title = title
     this.departureTime = departureTime
     this.departureLocation = departureLocation
@@ -15,7 +16,7 @@ export default class Carpool {
     this.driverGTID = driverGTID
     this.userIDs = userIDs
 
-    this.tripStatus = tripStatus
+    this.tripStatus = myTripStatus
     this.isTransactionFinished = isTransactionFinished
   }
 
